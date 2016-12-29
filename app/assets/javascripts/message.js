4bbde83bf9488a6a1ee1f23a5c171c99964b01b7
Jacob Green <jacobkg@gmail.com>
2016-12-29 22:19:13 +0000

    LocalStorage for username; color substitutions

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,10 +1,14 @@
 $( document ).ready(function() {
+
+  $('#username').val(localStorage.username)
+
   window.addEventListener("focus", function(event) {
     document.title = "PiChat";
   });
 
   $('#message-form').submit(function() {
     var username = $('#username').val();
+    localStorage.username = username;
     var body = $('#message').val();
     var font = $('#font').val();
     var date = new Date().toTimeString().substr(0,5)
--- app/channels/chat_channel.rb
+++ app/channels/chat_channel.rb
@@ -5,6 +5,7 @@ class ChatChannel < ApplicationCable::Channel
   end
 
   def receive(data)
+    data['body'].gsub!(/\[[dD](\d{1,2})\]/) {|x| "<span style=\"color: #4ca9d4\">#{SecureRandom.random_number(x[$1].to_i) + 1}</span>" }
     $chats ||= []
     $chats << data
     ActionCable.server.broadcast("chat_home", data)
